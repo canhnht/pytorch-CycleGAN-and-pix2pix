6adc28d9560117a666905baaa05a835b074f5201
Blue Nguyen <thanhcanh@acworks.co.jp>
2020-10-05 19:34:10 +0900

    Update

--- scripts/custom_test.sh
+++ scripts/custom_test.sh
@@ -8,6 +8,8 @@
 !CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name nicholas-roerich-20200921 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
 !CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name nicholas-roerich-20200914 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
 
+!CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name berthe-morisot-20200929 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
+
 # 1
 !CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name vangogh-20200914 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
 
@@ -30,3 +32,5 @@
 !CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name sesshu-toyo-20200914 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
 
 !CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name wassily-20200907 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
+
+!CUDA_VISIBLE_DEVICES=0 python test.py --dataroot datasets/test-custom/sports-256 --name cezanne-20200929 --model test --no_dropout --preprocess scale_width --load_size 2000 --eval --gpu_ids -1
